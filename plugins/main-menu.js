e3e2b25bf78d55d12c3674325517b976dbda986b
YALLICO OWNER <yallico2024@gmail.com>
2025-06-25 10:56:15 +0200

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,127 +1,256 @@
+import { promises } from 'fs'
+import { join } from 'path'
+import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
-
-const textCyberpunk = (text) => {
-  const charset = {
-    a: 'ᴀ', b: 'ʙ', c: 'ᴄ', d: 'ᴅ', e: 'ᴇ', f: 'ꜰ', g: 'ɢ',
-    h: 'ʜ', i: 'ɪ', j: 'ᴊ', k: 'ᴋ', l: 'ʟ', m: 'ᴍ', n: 'ɴ',
-    o: 'ᴏ', p: 'ᴘ', q: 'ǫ', r: 'ʀ', s: 'ꜱ', t: 'ᴛ', u: 'ᴜ',
-    v: 'ᴠ', w: 'ᴡ', x: 'x', y: 'ʏ', z: 'ᴢ'
-  }
-  return text.toLowerCase().split('').map(c => charset[c] || c).join('')
-}
-
+let Styles = (text, style = 1) => {
+  var xStr = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+  var yStr = Object.freeze({
+    1: 'ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ1234567890'
+  });
+  var replacer = [];
+  xStr.map((v, i) => replacer.push({
+    original: v,
+    convert: yStr[style].split('')[i]
+  }));
+  var str = text.toLowerCase().split('');
+  var output = [];
+  str.map(v => {
+    const find = replacer.find(x => x.original == v);
+    find ? output.push(find.convert) : output.push(v);
+  });
+  return output.join('');
+};
 let tags = {
-  'main': textCyberpunk('sistema'),
-  'group': textCyberpunk('grupos'),
+  'freefire':  '🎮 _ꜰʀᴇᴇ ꜰɪʀᴇ_ 🎮',
+  'freefireeu':  '🎮 _ꜰʀᴇᴇ ꜰɪʀᴇ ᴇᴜ_ 🎮',
+  'main': '❗ _ɪɴꜰᴏ_ ❕',
+  'search': '🔎 _ꜱᴇᴀʀᴄʜ_ 🔍',
+  'game': '🕹️ _ɢᴀᴍᴇ_ 🎮',
+  'rpg': '🌐 _ʀᴘɢ_ 🥇',
+  'rg': '🎑 _ʀᴇɢɪꜱᴛʀᴏ_ 🎟️',
+  'sticker': '💟 _ꜱᴛɪᴄᴋᴇʀ_ 🏷️',
+  'img': '🖼️ _ɪᴍᴀɢᴇɴ_ 🎇',
+  'group': '👥 _ɢʀᴜᴘᴏ_ 📢',
+//  'logo': '_MAKER_',
+  'nable': '🎛️ _ᴏɴ / ᴏꜰꜰ_ 🔌', 
+  'premium': '💎 _ᴘʀᴇᴍɪᴜɴ_ 👑',
+  'downloader': '📥 _ᴅᴇꜱᴄᴀʀɢᴀꜱ_ 📤',
+  'tools': '🔧 _ᴛᴏᴏʟꜱ_ 🛠️',
+  'fun': '🎉 _ꜰᴜɴ_ 🎊',
+  'nsfw': '🔞 _ɴꜱꜰᴡ_ 📛', 
+  'cmd': '🧮 _ᴅᴀᴛᴀ ʙᴀꜱᴇ_ 🖥️',
+  'owner': '👤 _ᴏᴡɴᴇʀ_ 👁️', 
+  'audio': '📣 _ᴀᴜᴅɪᴏꜱ_ 🔊', 
+  'advanced': '🗝️ _ᴀᴅᴠᴀɴᴄᴇ_ 📍',
+  'anime': '🧧 _ᴀɴɪᴍᴇ_ 🎐',
 }
 
 const defaultMenu = {
-  before: `╭─[𝗘𝗦𝗧𝗔𝗗𝗢 𝗗𝗘 𝗨𝗦𝗨𝗔𝗥𝗜𝗢]─╮
-│ 📊 𝗡𝗜𝗩𝗘𝗟: %level
-│ ⚡ 𝗘𝗫𝗣: %exp / %maxexp
-│ 🧮 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦: %totalreg
-│ ⏱ 𝗧𝗜𝗘𝗠𝗣𝗢 𝗔𝗖𝗧𝗜𝗩𝗢: %muptime
-╰──────────────────╯
+  before:  `*─ׄ─ׅ─⭒─ׄ─ׄ─⭒─ׅ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׄ─*
 
-🧬 *𝗠𝗢𝗗𝗢 𝗛𝗔𝗖𝗞 𝗔𝗖𝗧𝗜𝗩𝗔𝗗𝗢*
-*✦ Elige un comando para ejecutar protocolo :*
-*✦ Ejemplo :* .comprarbot
-*✦ Operador:* *Yallico Owner 👑*
-%readmore`.trimStart(),
+Hola *%name* Soy *Mafia*
 
-  header: '╭─〔 🦠 %category 〕─╮',
-  body: '│ ⚙️ %cmd',
-  footer: '╰────────────────╯',
-  after: '\n⌬ 𝐒𝐓𝐘𝐋𝐄 𝐃𝐑𝐈𝐅𝐓 ☠️ - Sistema ejecutado con éxito.'
+╔══════ •『 𝑪𝑹𝑬𝑨𝑫𝑶𝑹 』
+║  🖥️ Mafia
+╚═════ ♢.✰.♢ ══════
+╔══════ •『 𝑰𝑵𝑭𝑶-𝑩𝑶𝑻 』
+║  👤 Cliente: %name
+║  ⭐ Exp: %exp
+║  ⚡ Nivel: %level
+╚═════ ♢.✰.♢ ═══════
+
+╔══════ •『 𝑰𝑵𝑭𝑶-𝑼𝑺𝑬𝑹』
+║  🤖 Bot: ©Bot Mafia
+║  💎 Modo: Privado
+║  💨 Baileys: Multi Device
+║  🎩 Usuarios: %totalreg 
+╚═════ ♢.✰.♢ ════════
+
+*─ׄ─ׄ─⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׅ─*
+ %readmore
+\t\t\t⚙️_*𝐋𝐈𝐒𝐓𝐀 𝐃𝐄 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒*_ 🚀
+`.trimStart(),
+  header: '*╭╍╍╍╍❖【 %category 】*',
+  body: '*┋💎›【 %cmd* %islimit %isPremium\n',
+  footer: '*╰╍╍╍╍❖•ೋ° °ೋ•❖╍╍╍╍╯*',
+  after: `© ${textbot}`,
 }
 
-let handler = async (m, { conn, usedPrefix: _p }) => {
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
-    let tag = `@${m.sender.split("@")[0]}`
-    let { exp, level } = global.db.data.users[m.sender]
+     let tag = `@${m.sender.split("@")[0]}`
+    let mode = global.opts["self"] ? "Privado" : "Publico"
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let { exp, limit, level } = global.db.data.users[m.sender]
     let { min, xp, max } = xpRange(level, global.multiplier)
     let name = await conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'es'
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
     let _uptime = process.uptime() * 1000
-    let muptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let mode = global.opts["self"] ? "Privado" : "Público"
-
-    let help = Object.values(global.plugins).filter(p => !p.disabled).map(p => ({
-      help: Array.isArray(p.help) ? p.help : [p.help],
-      tags: Array.isArray(p.tags) ? p.tags : [p.tags],
-      prefix: 'customPrefix' in p,
-      limit: p.limit,
-      premium: p.premium,
-      enabled: !p.disabled,
-    }))
-
-    for (let plugin of help) {
-      if (plugin.tags) {
-        for (let t of plugin.tags) {
-          if (!(t in tags) && t) tags[t] = textCyberpunk(t)
-        }
-      }
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
     }
-
-    const { before, header, body, footer, after } = defaultMenu
-
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        limit: plugin.limit,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+    })
+    for (let plugin of help)
+      if (plugin && 'tags' in plugin)
+        for (let tag of plugin.tags)
+          if (!(tag in tags) && tag) tags[tag] = tag
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
     let _text = [
       before,
       ...Object.keys(tags).map(tag => {
-        const cmds = help
-          .filter(menu => menu.tags.includes(tag))
-          .map(menu => menu.help.map(cmd => body.replace(/%cmd/g, menu.prefix ? cmd : _p + cmd)).join('\n'))
-          .join('\n')
-        return `${header.replace(/%category/g, tags[tag])}\n${cmds}\n${footer}`
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%islimit/g, menu.limit ? '◜⭐◞' : '')
+                .replace(/%isPremium/g, menu.premium ? '◜🪪◞' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
       }),
       after
     ].join('\n')
+    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+   let replace = {
+ "%": "%",
+ p: _p,
+ uptime,
+ muptime,
+ me: conn.getName(conn.user.jid),
+ npmname: _package.name,
+ npmdesc: _package.description,
+ version: _package.version,
+ exp: exp - min,
+ maxexp: xp,
+ totalexp: exp,
+ xp4levelup: max - exp,
+ github: _package.homepage ? _package.homepage.url || _package.homepage : "[unknown github url]",
+ mode,
+ _p,
+ tag,
+ name,
+ level,
+ limit,
+ name,
+ totalreg,
+ readmore: readMore
+   }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-    let replace = {
-      '%': '%',
-      name,
-      level,
-      exp: exp - min,
-      maxexp: xp,
-      totalreg,
-      mode,
-      muptime,
-      readmore: String.fromCharCode(8206).repeat(4001)
-    }
-
-    let text = _text.replace(/%(\w+)/g, (_, key) => replace[key] || '')
-
-    await conn.sendMessage(m.chat, {
-    text: `⌬ 📡 ᴄʏʙᴇʀ ᴍᴇɴᴜ sʏsᴛᴇᴍ ɪɴɪᴄɪᴀɴᴅᴏ...\n⚙️ Cargando comandos...`,
-      mentions: [m.sender]
-    }, { quoted: m })
-
-    await conn.sendMessage(m.chat, {
-      image: { url: 'https://files.catbox.moe/0mervt.jpeg' },
-      caption: text,
-      footer: '🧠 STYLE DRIFT SYSTEM ☘️',
-      buttons: [
-        { buttonId: `${_p}grupos`, buttonText: { displayText: '🌐 ＧＲＵＰＯＳ ＳＴＹＬＥ' }, type: 1 },
-        { buttonId: `${_p}ventas`, buttonText: { displayText: '☘️ ＳＴＹＬＥ ＶＥＮＴＡＳ' }, type: 1 }
-      ],
-      viewOnce: true
-    }, { quoted: m })
+    let pp = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp2 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp3 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp4 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp5 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp6 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp7 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp8 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp9 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp10 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp11 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp12 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp13 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp14 = 'https://files.catbox.moe/0mervt.jpeg'
+    let pp15 = 'https://files.catbox.moe/0mervt.jpeg'
+    let img = 'https://files.catbox.moe/0mervt.jpeg'
+    let img2 = 'https://files.catbox.moe/0mervt.jpeg'
+    await m.react('⭐')
+   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
 
   } catch (e) {
-    console.error(e)
-    conn.reply(m.chat, '❎ Error al generar el menú del sistema.', m)
+    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
+    throw e
   }
 }
 
-handler.help = ['menu', 'menú']
+handler.help = ['allmenu']
 handler.tags = ['main']
-handler.command = ['menu', 'menú', 'help', 'ayuda']
-handler.register = true
+handler.command = ['allmenu', 'menucompleto', 'menúcompleto', 'menú', 'menu'] 
+handler.register = false 
 export default handler
+
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
 
 function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
+
+  var ase = new Date();
+  var hour = ase.getHours();
+switch(hour){
+  case 0: hour = 'una linda noche 🌙'; break;
+  case 1: hour = 'una linda noche 💤'; break;
+  case 2: hour = 'una linda noche 🦉'; break;
+  case 3: hour = 'una linda mañana ✨'; break;
+  case 4: hour = 'una linda mañana 💫'; break;
+  case 5: hour = 'una linda mañana 🌅'; break;
+  case 6: hour = 'una linda mañana 🌄'; break;
+  case 7: hour = 'una linda mañana 🌅'; break;
+  case 8: hour = 'una linda mañana 💫'; break;
+  case 9: hour = 'una linda mañana ✨'; break;
+  case 10: hour = 'un lindo dia 🌞'; break;
+  case 11: hour = 'un lindo dia 🌨'; break;
+  case 12: hour = 'un lindo dia ❄'; break;
+  case 13: hour = 'un lindo dia 🌤'; break;
+  case 14: hour = 'una linda tarde 🌇'; break;
+  case 15: hour = 'una linda tarde 🥀'; break;
+  case 16: hour = 'una linda tarde 🌹'; break;
+  case 17: hour = 'una linda tarde 🌆'; break;
+  case 18: hour = 'una linda noche 🌙'; break;
+  case 19: hour = 'una linda noche 🌃'; break;
+  case 20: hour = 'una linda noche 🌌'; break;
+  case 21: hour = 'una linda noche 🌃'; break;
+  case 22: hour = 'una linda noche 🌙'; break;
+  case 23: hour = 'una linda noche 🌃'; break;
+}
+  var greeting = "espero que tengas " + hour;
